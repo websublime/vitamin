57af4baad4dd399c242d87aba57dd536be7ddd98
VitaminBot <hello@websublime.dev>
2021-04-13 00:00:36 +0000

    website deploy

---
+++ assets/config.scene.a646e06a.js
@@ -0,0 +1,1 @@
+import{o as e,c as n}from"./index.de57f527.js";import"./vendor.49b999e3.js";const o={};o.render=function(o,r){return e(),n("h1",null,"Config")};export default o;
---
+++ assets/vit-light.6c6995fb.js
@@ -0,0 +1,1 @@
+var a="/vitamin/assets/vit-dark.fc45b571.png",s="/vitamin/assets/vit-light.23617cb4.png";export{a as _,s as a};
