c7f75facdd6782b724fd4500dd83ef5148702341
Miguel Ramos <miguel.ramos@websublime.dev>
2023-07-04 23:32:31 +0100

    feat: release

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,3 +1,13 @@
+/**
+|--------------------------------------------------------------------------
+| Copyright Websublime All Rights Reserved.
+|--------------------------------------------------------------------------
+|
+| Use of this source code is governed by an MIT-style license that can be
+| found in the LICENSE file at https://websublime.dev/license
+|
+*/
+
 declare const VERSION: string;
 
 const version = VERSION;
