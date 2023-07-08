2c0efb88743933e83121fe436536aac8b06282ab
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2023-07-08 11:57:14 +0100

    feat: starlight configurations

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -2,32 +2,48 @@ import { defineConfig } from 'astro/config';
 // eslint-disable-next-line import/no-unresolved
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   base: '/vitamin',
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
   integrations: [
     starlight({
-      sidebar: [
+      customCss: ['/src/theme/base.css'],
+      sidebar: [{
+        items: [
+        // Each item here is one entry in the navigation menu.
         {
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-          label: 'Guides'
+          label: 'Example Guide',
+          link: '/guides/example/'
+        }],
+        label: 'Guides'
+      }, {
+        autogenerate: {
+          directory: 'reference'
         },
-        {
-          autogenerate: { directory: 'reference' },
-          label: 'Reference'
-        }
-      ],
+        label: 'Reference'
+      }],
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: 'https://websublime.github.io/vitamin'
       },
-      title: 'My Docs'
-    })
+      title: 'Vitamin UI'
+    }),
+    tailwind({ applyBaseStyles: false, config: './tailwind.config.cjs' }),
+    {
+      hooks: {
+        'astro:config:setup': ({ injectScript }) => {
+          injectScript('page', `document.getElementsByTagName('html')[0].setAttribute('theme', 'orizon');`);
+        }
+      },
+      name: '@sublime/globals'
+    }
   ],
-
   site: 'https://websublime.github.io'
 });
