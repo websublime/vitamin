9ad30b8f4aba68c8bd35c16bf9ed1c270ced0a14
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2023-08-18 17:03:23 +0100

    feat(theming): defined new theming, colors and css vars

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -13,28 +13,26 @@ import { readFileSync } from 'node:fs';
 import { join, resolve } from 'node:path';
 // import { fileURLToPath } from 'node:url';
 
-import convert from 'color-convert';
+//import convert from 'color-convert';
 import { parse } from 'postcss';
 import postcssJs from 'postcss-js';
 import plugin from 'tailwindcss/plugin.js';
 
-import { palette } from './colors.js';
-import { defineTheme } from './themes.js';
+//import { palette } from './colors.js';
+import { defineRoot, defineThemes } from './themes.js';
 
 // eslint-disable-next-line max-len
 // const currentDirectory = typeof __dirname === undefined ? dirname(fileURLToPath(import.meta.url)) : __dirname;
 const currentDirectory = __dirname;
-const hslToString = ([hue, saturation, lightness]) => `${hue}deg ${saturation}% ${lightness}%`;
+/*const hslToString = ([hue, saturation, lightness]) => `${hue}deg ${saturation}% ${lightness}%`;
 
 const setupTheme = (themeDescriptor) => {
   const base = Object.create(null);
   const { colors, globals } = themeDescriptor;
 
-  for (const [key, value] of Object.entries(colors)) {
+  for (const [key, value] of Object.entries<string>(colors)) {
     const [red, green, blue] = convert.hex.rgb(value);
-    const intense = Math.round(
-      (Number.parseInt(red) * 299 + Number.parseInt(green) * 587 + Number.parseInt(blue) * 114) / 1000
-    );
+    const intense = Math.round((red * 299 + green * 587 + blue * 114) / 1000);
 
     base[`--${key}-color`] = hslToString(convert.hex.hsl(value));
     base[`--${key}-contrast`] =
@@ -46,7 +44,7 @@ const setupTheme = (themeDescriptor) => {
   }
 
   return base;
-};
+};*/
 
 export default plugin.withOptions(
   () => {
@@ -70,18 +68,43 @@ export default plugin.withOptions(
       const propertiesCss = readFileSync(resolve(join(currentDirectory, './css/utilities/properties.css')), 'utf8');
       const propertiesResult = postcssJs.objectify(parse(propertiesCss));
 
-      const defaults = setupTheme(defineTheme({ colors, globals }));
+      const { base, colors: rootColors, globals: rootGlobals, media, root } = defineRoot({ colors, globals });
 
-      addBase([{ [`[theme=${name}]`]: defaults }, { ...globalsResult }]);
+      // eslint-disable-next-line unicorn/no-array-reduce
+      const defaults = Object.entries({ ...base, ...rootColors, ...rootGlobals, ...root }).reduce(
+        (accumulator, [key, value]) => {
+          accumulator[`--${key}`] = value;
+          return accumulator;
+        },
+        {}
+      );
+
+      const { gray, red } = defineThemes();
+
+      addBase([
+        {
+          '.dark': {
+            '.theme-gray': gray?.dark,
+            '.theme-red': red?.dark
+          },
+          '.light': {
+            '.theme-gray': gray?.light,
+            '.theme-red': red?.light
+          },
+          ':root': {
+            ...defaults
+          },
+          ...media
+        },
+        { ...globalsResult }
+      ]);
       addUtilities([{ ...colorsResult }, { ...helpersResult }, { ...propertiesResult }]);
     };
   },
   function (): any {
     return {
       theme: {
-        extend: {
-          colors: palette
-        }
+        extend: {}
       },
       vitamin: {
         theme: {
